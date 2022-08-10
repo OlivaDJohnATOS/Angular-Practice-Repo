2dd1a9f272823198781094d4e7aa833832f53227
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-10 14:12:46 -0700

    Services and Dependency Injections

---
+++ services-start/src/app/new-account/new-account.component.ts
@@ -0,0 +1,30 @@
+import { AccountsService } from './../account.service';
+import { LoggingService } from './../logging.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+  //providers: [LoggingService]
+})
+export class NewAccountComponent {
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService){
+      this.accountsService.statusUpdated.subscribe(
+        (status: string) => alert('New Status: ' + status)
+      );
+    }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountsService.addAccount(accountName, accountStatus)
+    //this.loggingService.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
+  }
+}
